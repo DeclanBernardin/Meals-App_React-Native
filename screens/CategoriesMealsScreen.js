570d1d29f5a56712cd1420c7b0397d68db6fb5a5
Declan Bernardin <dmjbernardin@yahoo.com>
2020-01-23 14:07:01 -0600

    added another screen of navigation

--- screens/CategoriesMealsScreen.js
+++ screens/CategoriesMealsScreen.js
@@ -1,10 +1,13 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
 
 const CategoriesMealsScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>The Categories Meals Screen </Text>
+            <Button title="go to details" onPress={() => {
+                props.navigation.navigate({routeName: 'MealDetail'}); 
+            }}/>
         </View>
     );
 };
