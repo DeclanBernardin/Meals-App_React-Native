1d62382cac6cf32fb8e1fbb7a10c700155c2074e
Declan Bernardin <dmjbernardin@yahoo.com>
2020-01-23 20:31:08 -0600

    added some changes to the categories page

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,15 +1,19 @@
 import React from 'react';
-import { View, Text, Button,  StyleSheet} from 'react-native';
+import { View, Text, FlatList,  StyleSheet} from 'react-native';
+
+import { CATEGORIES } from '../data/dummy-data'; 
+
+const renderGridItem = (itemData) => {
+    return( 
+    <View style={styles.gridItem}>
+        <Text>{itemData.item.title}</Text>
+    </View>
+    );
+};
 
 const CategoriesScreen = props => {
-    console.log(props); 
     return (
-    <View style={styles.screen}>
-        <Text>The Categories Screen </Text>
-        <Button title="Go To Meals" onPress={() => {props.navigation.navigate({routeName: 'CategoryMeals'}); 
-     }}/> 
-        
-    </View>
+    <FlatList data={ CATEGORIES } renderItem={renderGridItem} numColumns={2} />
     );
 };
 
@@ -18,6 +22,11 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center', 
         alignItems: 'center' 
+    },
+    gridItem: {
+        flex: 1,
+        margin: 15,
+        height: 150, 
     }
 });
 
