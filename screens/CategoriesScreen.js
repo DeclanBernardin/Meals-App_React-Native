3d93167972b98738cfae94e9f1ccfdb5b7930730
Declan Bernardin <dmjbernardin@yahoo.com>
2020-01-23 20:59:26 -0600

    added some styling changes to the header of the meal categories

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -1,21 +1,37 @@
 import React from 'react';
-import { View, Text, FlatList,  StyleSheet} from 'react-native';
+import { View, Text, FlatList,  StyleSheet, TouchableOpacity, Platform} from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data'; 
+import Colors from '../constants /Colors'
+import { SyntheticPlatformEmitter } from '@unimodules/core';
 
-const renderGridItem = (itemData) => {
-    return( 
-    <View style={styles.gridItem}>
-        <Text>{itemData.item.title}</Text>
-    </View>
-    );
-};
+
 
 const CategoriesScreen = props => {
+    const renderGridItem = (itemData) => {
+        return (
+            <TouchableOpacity style={styles.gridItem} onPress={() => {
+                props.navigation.navigate({routeName: 'CategoryMeals'});
+            }}>
+                <View>
+                    <Text>{itemData.item.title}</Text>
+                </View>
+            </TouchableOpacity>
+        );
+    };
+
     return (
     <FlatList data={ CATEGORIES } renderItem={renderGridItem} numColumns={2} />
     );
 };
+
+CategoriesScreen.navigationOptions = {
+    headerTitle: 'Meal Categories',
+    headerStyle: {
+        backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+    },
+    headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
+}; 
 
 const styles = StyleSheet.create({
     screen: {
