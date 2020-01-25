646e3a6e7aca44bd2bb3207dc525f2c2b39bfc28
Declan Bernardin <dmjbernardin@yahoo.com>
2020-01-24 20:43:44 -0600

    error fix and styling changes to CMS file

--- screens/CategoriesMealsScreen.js
+++ screens/CategoriesMealsScreen.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Platform } from 'react-native';
 
 import { CATEGORIES } from '../data/dummy-data'; 
+import Colors from '../constants /Colors'
 
 const CategoriesMealsScreen = props => {
     const catId = props.navigation.getParam('categoryId')
@@ -28,7 +29,11 @@ CategoriesMealsScreen.navigationOptions = navigationData => {
     const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
 
     return {
-        headerTitle: selectCategory.title
+        headerTitle: selectedCategory.title,
+        headerStyle: {
+            backgroundColor: Platform.OS === 'android' ? Colors.primaryColor : ''
+        },
+        headerTintColor: Platform.OS === 'android' ? 'white' : Colors.primaryColor,
     }; 
 }; 
 
