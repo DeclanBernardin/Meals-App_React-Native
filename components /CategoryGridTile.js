bf317bf4a070ab379a930f370a355ae14b1823a1
Declan Bernardin <dmjbernardin@yahoo.com>
2020-01-27 13:59:55 -0600

    added recipies

--- components /CategoryGridTile.js
+++ components /CategoryGridTile.js
@@ -9,11 +9,11 @@ const CategoryGridTitle = props => {
     }
     return (
         <View style={styles.gridItem}>
-    <TouchableCmp style={{flex: 1}} onPress={props.onSelect}>
-        <View style={{ ...styles.container, ...{backgroundColor: props.color}}}>
-            <Text style={styles.title}>{props.title}</Text>
-        </View>
-    </TouchableCmp>
+            <TouchableCmp style={{flex: 1}} onPress={props.onSelect}>
+                <View style={{ ...styles.container, ...{backgroundColor: props.color}}}>
+                    <Text style={styles.title}>{props.title}</Text>
+                </View>
+            </TouchableCmp>
         </View>
     );
 };
@@ -39,7 +39,7 @@ const styles =StyleSheet.create({
         alignItems: 'flex-end'
     },
     title: {
-        fontFamily: 'open-sans-bold',
+        // fontFamily: 'open-sans-bold',
         fontSize: 20,
         textAlign: 'right',
     }
