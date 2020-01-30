f682200f142e0b184c9e0ba304f484da13b08a7e
Declan Bernardin <dmjbernardin@yahoo.com>
2020-01-29 18:02:16 -0600

    added ome more filters

--- components /MealItem.js
+++ components /MealItem.js
@@ -60,6 +60,7 @@ const styles = StyleSheet.create({
         fontSize: 20,
         color: 'white',
         textAlign: 'center',
+        fontWeight: 'bold',
     }
 });
 
--- screens/MealDetailScreen.js
+++ screens/MealDetailScreen.js
@@ -63,7 +63,7 @@ const styles = StyleSheet.create({
         fontFamily: 'open-sans',
         fontSize: 22,
         textAlign: 'center',
-        fontWeight: 'bold',
+        
     },
     listItem: {
         marginVertical: 10,
